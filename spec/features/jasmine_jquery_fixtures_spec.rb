afe0f394b08468a99b6df36f3d3f49746d15e2cb
aviflombaum <avi@flombaum.com>
2015-02-14 18:22:30 -0500

    Modified runner to move option parsing to allow for test run of bin

--- spec/features/jasmine_jquery_fixtures_spec.rb
+++ spec/features/jasmine_jquery_fixtures_spec.rb
@@ -1,7 +1,7 @@
 describe "Running a Jasmine jQuery Specs with Fixtures" do
   context 'without a browser through PhantomJS' do
     it 'runs the spec with 0 failures' do
-      output = `cd ./spec/fixtures/jasmine-jquery-fixtures && ./../../../bin/ironboard --local`
+      output = `cd ./spec/fixtures/jasmine-jquery-fixtures && ./../../../bin/ironboard --local --test`
       
       expect(output).to include('1 spec, 0 failures')
       expect(output).to_not include('1 failures')
