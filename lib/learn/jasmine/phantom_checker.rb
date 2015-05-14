9aaf041a85f8f896015650995c5551fa164561f1
Logan Hasson <logan.hasson@gmail.com>
2015-05-14 11:16:53 -0400

    Update gem to work on linux

--- lib/learn/jasmine/phantom_checker.rb
+++ lib/learn/jasmine/phantom_checker.rb
@@ -6,12 +6,18 @@ module Learn
       end
 
       def check_installation
-        if !brew_installed?
-          puts "You must have Homebrew installed."
-          exit
+        if running_on_mac?
+          if !brew_installed?
+            puts "You must have Homebrew installed."
+            exit
+          else
+            if !phantom_installed_on_mac?
+              install_phantomjs
+            end
+          end
         else
-          if !phantom_installed?
-            install_phantomjs
+          if !phantom_installed_on_linux?
+            puts "You must have PhantomJS installed: http://phantomjs.org/download.html"
           end
         end
       end
@@ -20,12 +26,28 @@ module Learn
         !`which brew`.empty?
       end
 
-      def phantom_installed?
+      def phantom_installed_on_mac?
+        phantom_installed_by_brew? || phantom_installed?
+      end
+
+      def phantom_installed_on_linux?
+        phantom_installed?
+      end
+
+      def phantom_installed_by_brew?
         !`brew ls --versions phantomjs`.empty?
+      end
+
+      def phantom_installed?
+        !`which phantomjs`.empty?
       end
 
       def install_phantomjs
         `brew install phantomjs`
+      end
+
+      def running_on_mac?
+        !!RUBY_PLATFORM.match(/darwin/)
       end
     end
   end
