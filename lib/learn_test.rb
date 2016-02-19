c3ec4e7f322866ed7f3ea0611b58cfc9d06a4908
Josh Rowley <joshua.f.rowley@gmail.com>
2016-02-18 21:06:38 -0500

    require relative for easier local work

--- lib/learn_test.rb
+++ lib/learn_test.rb
@@ -2,25 +2,24 @@ require 'fileutils'
 require 'faraday'
 require 'oj'
 
-require 'learn_test/version'
-require 'learn_test/netrc_interactor'
-require 'learn_test/github_interactor'
-require 'learn_test/user_id_parser'
-require 'learn_test/username_parser'
-require 'learn_test/repo_parser'
-require 'learn_test/file_finder'
-require 'learn_test/spec_type_parser'
+require_relative 'learn_test/version'
+require_relative 'learn_test/netrc_interactor'
+require_relative 'learn_test/github_interactor'
+require_relative 'learn_test/user_id_parser'
+require_relative 'learn_test/username_parser'
+require_relative 'learn_test/repo_parser'
+require_relative 'learn_test/file_finder'
+require_relative 'learn_test/spec_type_parser'
 
-require 'learn_test/rspec/runner'
+require_relative 'learn_test/rspec/runner'
 
-require 'learn_test/jasmine/phantom_checker'
-require 'learn_test/jasmine/initializer'
-require 'learn_test/jasmine/runner'
+require_relative 'learn_test/jasmine/phantom_checker'
+require_relative 'learn_test/jasmine/initializer'
+require_relative 'learn_test/jasmine/runner'
 
-require 'learn_test/python_unittest/requirements_checker'
-require 'learn_test/python_unittest/nose_installer'
-require 'learn_test/python_unittest/runner'
+require_relative 'learn_test/python_unittest/requirements_checker'
+require_relative 'learn_test/python_unittest/nose_installer'
+require_relative 'learn_test/python_unittest/runner'
 
 module LearnTest
 end
-
