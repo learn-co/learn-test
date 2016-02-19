b631975d801de6396c5f47b6e45fec1761b08391
Josh Rowley <joshua.f.rowley@gmail.com>
2016-02-19 00:18:18 -0500

    clean up and organize runner

--- lib/learn_test/runner.rb
+++ lib/learn_test/runner.rb
@@ -1,17 +1,26 @@
 module LearnTest
   class Runner
-    attr_reader :repo, :options, :strategy, :connection
+    attr_reader :repo, :options
 
     def initialize(repo, options = {})
       @repo = repo
       @options = options
-      @strategy = select_strategy.new(self)
+      die! if strategies.empty?
+    end
 
-      if !strategy
-        puts "This directory doesn't appear to have any specs in it."
-        exit
+    def run
+      strategies.each do |strategy|
+        strategy.check_dependencies
+        strategy.configure
+        strategy.run
+        if !help_option_present?
+          push_results(strategy)
+          strategy.cleanup unless keep_results?
+        end
       end
     end
+
+    private
 
     def connection
       @connection ||= Faraday.new(url: SERVICE_URL) do |faraday|
@@ -19,31 +28,11 @@ module LearnTest
       end
     end
 
-    def run
-      strategy.check_dependencies
-      strategy.configure
-      strategy.run
-      if !help_option_present?
-        push_results(strategy.results)
-        strategy.cleanup unless keep_results?
-      end
+    def strategies
+      @strategies ||= detect_strategies.map{ |s| s.new(self) }
     end
 
-    def push_results(results)
-      begin
-        connection.post do |req|
-          req.url(strategy.service_endpoint)
-          req.headers['Content-Type'] = 'application/json'
-          req.body = Oj.dump(results, mode: :compat)
-        end
-      rescue Faraday::ConnectionFailed
-        puts 'There was a problem connecting to Learn. Not pushing test results.'
-      end
-    end
-
-    private
-
-    def select_strategy
+    def detect_strategies
       spec_type = LearnTest::SpecTypeParser.new.spec_type
 
       strategies = {
@@ -52,7 +41,19 @@ module LearnTest
         python_unittest: LearnTest::Strategies::PythonUnittest
       }
 
-      strategies[spec_type.to_sym]
+      [strategies[spec_type.to_sym]]
+    end
+
+    def push_results(strategy)
+      begin
+        connection.post do |req|
+          req.url(strategy.service_endpoint)
+          req.headers['Content-Type'] = 'application/json'
+          req.body = Oj.dump(strategy.results, mode: :compat)
+        end
+      rescue Faraday::ConnectionFailed
+        puts 'There was a problem connecting to Learn. Not pushing test results.'
+      end
     end
 
     def keep_results?
@@ -61,6 +62,11 @@ module LearnTest
 
     def help_option_present?
       options.include?('-h') || options.include?('--help')
+    end
+
+    def exit!
+      puts "This directory doesn't appear to have any specs in it."
+      exit
     end
   end
 end
