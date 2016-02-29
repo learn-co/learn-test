cfc393dd81ef832545a24d15f0bc8692fee3f333
Logan Hasson <logan.hasson@gmail.com>
2016-02-29 12:09:17 -0500

    Some test running stuff

--- lib/learn_test/strategies/java_junit.rb
+++ lib/learn_test/strategies/java_junit.rb
@@ -1,3 +1,6 @@
+require 'crack'
+require 'json'
+
 module LearnTest
   module Strategies
     class JavaJunit < LearnTest::Strategy
@@ -19,10 +22,48 @@ module LearnTest
         make_json
       end
 
+      def results
+        @results ||= {
+          username: username,
+          github_user_id: user_id,
+          repo_name: runner.repo,
+          build: {
+            test_suite: [{
+              framework: 'junit',
+              formatted_output: [],
+              duration: 0.0
+            }]
+          },
+          examples: 0,
+          passing_count: 0,
+          failure_count: 0
+        }
+      end
+
+      def cleanup
+      end
+
       private
 
       def run_ant
+        system('ant test -buildfile javacs*/build.xml')
+        # output is in javacs*/junit/*.xml <-- need to combine those like in jasmine
+      end
 
+      def test_path
+        @test_path ||= File.expand_path("#{lab_dir}/junit", FileUtils.pwd)
+      end
+
+      def lab_dir
+        @lab_dir ||= Dir.entries('.').detect {|f| f.match(/^javacs\-lab\d+$/)}
+      end
+
+      def make_json
+
+      end
+
+      def test_xml_files
+        Dir.entries(test_path).select { |f| f.end_with?('.xml') }
       end
     end
   end
