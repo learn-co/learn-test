ec9da9670bda54c9c381fdb83cb16a6ca2902546
Logan Hasson <logan.hasson@gmail.com>
2016-05-27 14:48:41 -0400

    Don't require brew on linux

--- lib/learn_test/dependencies/nodejs.rb
+++ lib/learn_test/dependencies/nodejs.rb
@@ -6,17 +6,26 @@ module LearnTest
       end
 
       def install
-        puts('Checking for homebrew...'.green)
-        die('You must have Homebrew installed') unless brew_installed?
-        puts('Updating brew...'.green)
-        print_installing('node')
-        run_install('brew install node')
+        if !mac?
+          die('Please install NodeJS: https://nodejs.org/en/download')
+        else
+          puts('Checking for homebrew...'.green)
+          die('You must have Homebrew installed') unless brew_installed?
+          puts('Updating brew...'.green)
+          print_installing('node')
+          run_install('brew install node')
+        end
       end
 
       private
 
       def brew_installed?
         !`which brew`.empty?
+      end
+
+      def die(message)
+        puts message
+        exit
       end
     end
   end
--- lib/learn_test/version.rb
+++ lib/learn_test/version.rb
@@ -1,3 +1,3 @@
 module LearnTest
-  VERSION = '2.3.2'
+  VERSION = '2.3.3'
 end
