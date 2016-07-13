f87c0ad6d882d2ba206c8a9af52f646236694850
Logan Hasson <logan.hasson@gmail.com>
2016-07-13 16:14:33 -0400

    Pass along github key from dot_learn

--- lib/learn_test/runner.rb
+++ lib/learn_test/runner.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 module LearnTest
   class Runner
     attr_reader :repo, :options
@@ -33,6 +35,16 @@ module LearnTest
 
     private
 
+    def augment_results!(results)
+      if File.exist?("#{FileUtils.pwd}/.learn")
+        dot_learn = YAML.load(File.read("#{FileUtils.pwd}/.learn"))
+
+        if !dot_learn['github'].nil?
+          results[:github] = dot_learn['github']
+        end
+      end
+    end
+
     def connection
       @connection ||= Faraday.new(url: SERVICE_URL) do |faraday|
         faraday.adapter  Faraday.default_adapter
@@ -53,11 +65,14 @@ module LearnTest
     end
 
     def push_results(strategy)
+      results = strategy.results
+      augment_results!(results)
+
       begin
         connection.post do |req|
           req.url(strategy.service_endpoint)
           req.headers['Content-Type'] = 'application/json'
-          req.body = Oj.dump(strategy.results, mode: :compat)
+          req.body = Oj.dump(results, mode: :compat)
         end
       rescue Faraday::ConnectionFailed
         puts 'There was a problem connecting to Learn. Not pushing test results.'.red
