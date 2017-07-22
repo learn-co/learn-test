c386a9f2e839fba7813732856df4284ec7add9ed
Gabe Jackson <gj@mail.co.de>
2017-07-22 19:33:30 -0400

    Add back Node-based Mocha browser strategy and implement control flow until we finish migrating all of the Mocha labs to the new browser-based test strategy

--- lib/learn_test/strategies/mocha.rb
+++ lib/learn_test/strategies/mocha.rb
@@ -1,6 +1,8 @@
 module LearnTest
   module Strategies
     class Mocha < LearnTest::Strategy
+      attr_accessor :mocha_in_browser
+
       def service_endpoint
         '/e/flatiron_mocha'
       end
@@ -9,16 +11,34 @@ module LearnTest
         package = File.exist?('package.json') ? Oj.load(File.read('package.json'), symbol_keys: true) : nil
         return false if !package
 
-        if package[:scripts] && package[:scripts][:test]
-          return true if package[:scripts][:test].include? 'mocha'
+        if package[:scripts] && package[:scripts][:test] && package[:scripts][:test].include?('mocha')
+          self.mocha_in_browser = false
+
+          return true
         end
 
-        if package[:devDependencies] && package[:devDependencies][:mocha]
-          return true if (package[:devDependencies][:mocha].length > 0)
+        if package[:devDependencies]
+          if package[:devDependencies][:mocha]
+            self.mocha_in_browser = false
+
+            return true
+          elsif package[:devDependencies][:'learn-browser']
+            self.mocha_in_browser = true
+
+            return true
+          end
         end
 
-        if package[:dependencies] && package[:dependencies][:mocha]
-          return true if (package[:dependencies][:mocha].length > 0)
+        if package[:dependencies]
+          if package[:dependencies][:mocha]
+            self.mocha_in_browser = false
+
+            return true
+          elsif package[:dependencies][:'learn-browser']
+            self.mocha_in_browser = true
+
+            return true
+          end
         end
 
         return false
@@ -33,11 +53,38 @@ module LearnTest
       end
 
       def cleanup
-        FileUtils.rm('learn.auth.data.json') if File.exist?('learn.auth.data.json')
+        if mocha_in_browser
+          FileUtils.rm('learn.auth.data.json') if File.exist?('learn.auth.data.json')
+        else
+          FileUtils.rm('.results.json') if File.exist?('.results.json')
+        end
       end
 
       def push_results?
-        false
+        !mocha_in_browser
+      end
+
+      def results
+        @results ||= {
+          username: username,
+          github_user_id: user_id,
+          learn_oauth_token: learn_oauth_token,
+          repo_name: runner.repo,
+          build: {
+            test_suite: [{
+              framework: 'mocha',
+              formatted_output: output,
+              duration: output[:stats]
+            }]
+          },
+          examples: output[:stats][:tests],
+          passing_count: output[:stats][:passes],
+          failure_count: output[:stats][:failures]
+        }
+      end
+
+      def output
+        @output ||= File.exists?('.results.json') ? Oj.load(File.read('.results.json'), symbol_keys: true) : nil
       end
 
       private
@@ -47,22 +94,41 @@ module LearnTest
 
         npm_install(package)
 
+        if mocha_in_browser
+          run_browser_based_mocha
+        else
+          run_node_based_mocha(package)
+        end
+      end
+
+      def run_browser_based_mocha
         write_auth_data_to_file
 
-        puts "Navigate to #{testing_address} in your browser to run the test suite."
+        puts "Navigate to ".red + testing_address.blue + " in your browser to run the test suite.".red
         puts "As you write code in index.js, save your work often. With each save, the browser"
         puts "will automatically refresh and rerun the test suite against your updated code."
-        puts "To exit, press CTRL-C in the terminal."
+        puts "To exit the test suite and return to your terminal, press CTRL-C.".red
 
         begin
-          system("browser-sync start --config bs-config.js")
+          system("browser-sync start --config node_modules/learn-browser/bs-config.js")
         rescue Interrupt
-          puts "\nExiting test suite..."
+          puts "\nExiting test suite...".red
 
           cleanup
 
           exit
         end
+      end
+
+      def run_node_based_mocha(package)
+        command = if (package[:scripts] && package[:scripts][:test] || "").include?(".results.json")
+          "npm test"
+        else
+          install_mocha_multi
+          "node_modules/.bin/mocha -R mocha-multi --reporter-options spec=-,json=.results.json"
+        end
+
+        system(command)
       end
 
       def missing_dependencies?(package)
@@ -100,6 +166,12 @@ module LearnTest
 
       def testing_address
         in_IDE? ? "http://#{ENV['HOST_IP']}:#{ENV['PORT']}/" : "http://localhost:8000/"
+      end
+
+      def install_mocha_multi
+        if !File.exists?('node_modules/mocha-multi')
+          run_install('npm install mocha-multi', npm_install: true)
+        end
       end
     end
   end
