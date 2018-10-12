5fe0fc87c9ae9f224fefa2cb76f6cb2c3cd9cdcc
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-10-12 12:03:31 -0400

    removes class invoking an instance just to invoke an instance method then immediately discarding instance. Now, class calls class methods

--- lib/learn_test/strategies/python_unittest/requirements_checker.rb
+++ lib/learn_test/strategies/python_unittest/requirements_checker.rb
@@ -4,12 +4,9 @@ require 'open3'
 
 module LearnTest
   module PythonUnittest
+    
     class RequirementsChecker
       def self.check_installation
-        new.check_installation
-      end
-
-      def check_installation
         PythonChecker.check
         PipChecker.check
       end
@@ -17,21 +14,17 @@ module LearnTest
 
     class PythonChecker
       def self.check
-        new.check
-      end
-
-      def check
-        if !python_installed? || !correct_python_version?
+        if !self.python_installed? || !self.correct_python_version?
           puts "Please install python 2.7.x or 3.x.x"
           exit
         end
       end
 
-      def python_installed?
+      def self.python_installed?
         !`which python`.empty?
       end
 
-      def correct_python_version?
+      def self.correct_python_version?
         output = Open3.popen2e('python', '--version')
         version = output[1].read.strip
         !!version.match(/ 2.7.*| 3.*/)
@@ -40,17 +33,13 @@ module LearnTest
 
     class PipChecker
       def self.check
-        new.check
-      end
-
-      def check
-        if !pip_installed?
+        if !self.pip_installed?
           puts "Please ensure pip is installed"
           exit
         end
       end
 
-      def pip_installed?
+      def self.pip_installed?
         !`which pip`.empty?
       end
     end
