3c9e615188a54f4ae04ac1fc7298b0a710e328ea
Aspen James <aspenjames@tqca.org>
2018-12-22 20:01:59 -0800

    add spec for example flag

--- spec/features/rspec_unit_spec.rb
+++ spec/features/rspec_unit_spec.rb
@@ -3,8 +3,17 @@ describe "Running a RSpec Unit Test" do
     it 'runs the spec with 0 failures' do
       output = `cd ./spec/fixtures/rspec-unit-spec && ./../../../bin/learn-test --local --test`
 
-      expect(output).to include('1 example, 0 failures')
+      expect(output).to include('2 examples, 0 failures')
       expect(output).to_not include('1 failures')
+    end
+  end
+
+  context 'with the --example flag' do
+    it 'runs only the appropriate tests' do
+      output = `cd ./spec/fixtures/rspec-unit-spec && ./../../../bin/learn-test --local --test --example multiple`
+
+      expect(output).to include('1 example, 0 failures')
+      expect(output).to_not include('2 examples')
     end
   end
 end
--- spec/fixtures/rspec-unit-spec/spec/dog_spec.rb
+++ spec/fixtures/rspec-unit-spec/spec/dog_spec.rb
@@ -4,4 +4,8 @@ describe Dog do
   it 'runs the spec spec correctly' do
     expect(subject).to be_an_instance_of(Dog)
   end
+
+  it 'has multiple specs' do
+    expect(subject).not_to be_an_instance_of(String)
+  end
 end
