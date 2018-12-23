2937da4881f483d5d104b2abfa7de74d93075b05
Aspen James <aspenjames@tqca.org>
2018-12-22 20:31:53 -0800

    add simplecov for coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
 require_relative '../lib/learn_test'
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
