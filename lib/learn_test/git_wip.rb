d3e0791050cbfee3d5f04dda35653f9814e41f52
Tom Milewski <tmilewski@gmail.com>
2020-08-25 13:50:54 -0400

    Use Open3.popen3

--- lib/learn_test/git_wip.rb
+++ lib/learn_test/git_wip.rb
@@ -9,13 +9,26 @@ module LearnTest
         git = Git.open('./', log: log)
         working_branch = git.current_branch
 
-        `learn-test-wip save "Automatic test submission" -u &> /dev/null`
-
-        return false unless $?.success? # rubocop:disable Style/SpecialGlobalVars
-
-        git.push('origin', "wip/#{working_branch}:refs/heads/wip")
-        git.config['remote.origin.url'].gsub('.git', '/tree/wip')
-      rescue StandardError
+        Open3.popen3('./bin/learn-test-wip save "Automatic test submission" --editor') do |_stdin, _stdout, stderr, wait_thr|
+          # while out = stdout.gets do
+          #   puts out
+          # end
+  
+          while err = stderr.gets do
+            puts err
+          end
+  
+          if wait_thr.value.exitstatus.zero?
+            git.push('origin', "wip/#{working_branch}:refs/heads/wip")
+            git.config['remote.origin.url'].gsub('.git', '/tree/wip')
+            true
+          else
+            puts 'There was an error running learn-test-wip'
+            false
+          end
+        end
+      rescue StandardError => e
+        puts e
         false
       end
     end
