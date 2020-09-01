0eb490d3308a7f3d006deea4c39db131ab889cd6
Tom Milewski <tmilewski@gmail.com>
2020-09-01 10:29:07 -0400

    Add specs

--- lib/learn_test/git_wip.rb
+++ lib/learn_test/git_wip.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'git'
+require 'logger'
 
 module LearnTest
   module GitWip
@@ -9,25 +10,23 @@ module LearnTest
         git = Git.open('./', log: log)
         working_branch = git.current_branch
 
-        Open3.popen3('learn-test-wip save "Automatic test submission" --editor') do |_stdin, stdout, stderr, wait_thr|
-          while out = stdout.gets do
-            puts out
-          end
-  
-          while err = stderr.gets do
-            puts err
-          end
+        commands = [
+          'learn-test-wip save "Automatic test submission" --editor',
+          "git push origin wip/#{working_branch}:refs/heads/wip"
+        ].join(';')
+
+        Open3.popen3(commands) do |_stdin, _stdout, _stderr, wait_thr|
+          # while out = stdout.gets do; puts out; end
+          # while err = stderr.gets do; puts err; end
   
           if wait_thr.value.exitstatus.zero?
-            git.push('origin', "wip/#{working_branch}:refs/heads/wip")
             git.config['remote.origin.url'].gsub('.git', '/tree/wip')
           else
-            puts 'There was an error running learn-test-wip'
+            #  puts 'There was an error running learn-test-wip'
             false
           end
         end
       rescue StandardError => e
-        puts e
         false
       end
     end
--- spec/learn_test/git_spec.rb
+++ spec/learn_test/git_spec.rb
@@ -7,28 +7,35 @@ describe LearnTest::GitWip do
   let!(:git_url) { 'https://github.com/learn-co/learn-test' }
   let!(:git_base) { instance_double(Git::Base) }
 
+  let(:wait_thr) { double }
+  let(:wait_thr_value) { double }
+  let(:stdout_and_stderr) { double }
+
   context 'success' do
     it 'should return the git url' do
       expect(Git::Base).to receive(:open).with('./', { log: false }).and_return(git_base)
       expect(git_base).to receive(:current_branch).and_return(working_branch)
 
-      expect(subject).to receive(:`).with(/learn-test-wip save ".+" -u &> \/dev\/null/ )
-      expect($?).to receive(:success?).and_return(true)
+      expect(wait_thr).to receive(:value).and_return(wait_thr_value)
+      expect(wait_thr_value).to receive(:exitstatus).and_return(0)
 
-      expect(git_base).to receive(:push).with('origin', "wip/#{working_branch}:refs/heads/wip")
+      expect(Open3).to receive(:popen3).and_yield(nil, nil, nil, wait_thr)
+
       expect(git_base).to receive_message_chain(:config, :[]).with('remote.origin.url').and_return("#{git_url}.git")
-
       expect(subject.run!).to eq("#{git_url}/tree/wip")
     end
   end
 
   context 'failure' do
     it 'should return false on process error' do
-      allow(Git::Base).to receive(:open).and_return(git_base)
-      allow(git_base).to receive(:current_branch)
-      allow(subject).to receive(:`)
+      expect(Git::Base).to receive(:open).with('./', { log: false }).and_return(git_base)
+      expect(git_base).to receive(:current_branch).and_return(working_branch)
 
-      expect($?).to receive(:success?).and_return(false)
+      expect(wait_thr).to receive(:value).and_return(wait_thr_value)
+      expect(wait_thr_value).to receive(:exitstatus).and_return(1)
+
+      expect(Open3).to receive(:popen3).and_yield(nil, nil, nil, wait_thr)
+
       expect(subject.run!).to eq(false)
     end
 
